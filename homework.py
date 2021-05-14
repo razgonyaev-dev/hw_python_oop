2c3869130ce384118dfb86019bfa7757cc9ec03f
Denis <razgonyaev1@yandex.ru>
2021-05-14 13:05:13 +0300

    version3

--- homework.py
+++ homework.py
@@ -2,13 +2,12 @@ import datetime as dt
 
 
 class Calculator:
+    TODAY = dt.date.today()
+    WEEK = dt.timedelta(days=7)
 
     def __init__(self, limit):
         self.limit = limit
         self.records = []
-        self.today = dt.date.today()
-        self.week = dt.timedelta(days=7)
-        self.remained = self.limit - self.get_today_stats()
 
     def add_record(self, record):
         self.records.append(record)
@@ -16,17 +15,18 @@ class Calculator:
     def get_today_stats(self):
         return sum(
             rec.amount for rec in self.records if
-            rec.date == self.today)
+            rec.date == self.TODAY)
 
     def get_week_stats(self):
-        week_ago = self.today - self.week
+        week_ago = self.TODAY - self.WEEK
         return sum(
             rec.amount for rec in self.records if
-            week_ago < rec.date <= self.today)
+            week_ago < rec.date <= self.TODAY)
 
 
 class Record:
-    date_order_ru = '%d.%m.%Y'
+
+    DATE_FORMAT = '%d.%m.%Y'
 
     def __init__(self, amount, comment, date=None):
         self.amount = amount
@@ -34,21 +34,20 @@ class Record:
         if date is None:
             self.date = dt.date.today()
         else:
-            self.date = dt.datetime.strptime(date, self.date_order_ru).date()
+            self.date = dt.datetime.strptime(date, self.DATE_FORMAT).date()
 
 
 class CaloriesCalculator(Calculator):
-    stop_phrase = 'Хватит есть!'
-    cont_phrase = (
+    STOP_EATING = 'Хватит есть!'
+    YOU_CAN_EAT = (
         'Сегодня можно съесть что-нибудь ещё, '
-        'но с общей калорийностью не более {rem} кКал')
+        'но с общей калорийностью не более {remained} кКал')
 
     def get_calories_remained(self):
         remained = self.limit - self.get_today_stats()
         if remained > 0:
-            return self.cont_phrase.format(rem=remained)
-        else:
-            return self.stop_phrase
+            return self.YOU_CAN_EAT.format(remained=remained)
+        return self.STOP_EATING
 
 
 class CashCalculator(Calculator):
@@ -56,40 +55,39 @@ class CashCalculator(Calculator):
     USD_RATE = 60.0
     EURO_RATE = 70.0
 
-    curr_info = {
+    currency_exchanger = {
         'rub': [RUB_RATE, 'руб'],
         'usd': [USD_RATE, 'USD'],
         'eur': [EURO_RATE, 'Euro']}
 
-    stay_strong_phrase_debt = 'Денег нет, держись: твой долг - {cash} {title}'
-    stay_strong_phrase = 'Денег нет, держись'
-    you_have_money_phrase = 'На сегодня осталось {cash} {title}'
-    unknown_curr_phrase = 'Неизвестная валюта: "{curr}", попробуйте другую'
+    STAY_STRONG_WITH_DEBT = 'Денег нет, держись: твой долг - {cash} {title}'
+    STAY_STRONG = 'Денег нет, держись'
+    YOU_HAVE_MONEY = 'На сегодня осталось {cash} {title}'
+    UNKNOWN_CURRENCY = 'Неизвестная валюта: "{currency}", попробуйте другую'
 
     def get_today_cash_remained(self, currency='rub'):
-        if currency not in CashCalculator.curr_info.keys():
-            return self.unknown_curr_phrase.format(curr=currency)
+        if currency not in self.currency_exchanger:
+            raise KeyError(self.UNKNOWN_CURRENCY.format(currency=currency))
 
-        rate, title = CashCalculator.curr_info[currency]
+        rate, title = CashCalculator.currency_exchanger[currency]
 
         cash_remained = self.limit - self.get_today_stats()
-        no_cash_remained = abs(round(cash_remained / rate, 2))
+        out_cash_remained = round(cash_remained / rate, 2)
 
         if cash_remained < 0:
-            return self.stay_strong_phrase_debt.format(
-                cash=no_cash_remained, title=title)
-        if cash_remained == 0:
-            return self.stay_strong_phrase
-        else:
-            return self.you_have_money_phrase.format(
-                cash=no_cash_remained, title=title)
+            return self.STAY_STRONG_WITH_DEBT.format(
+                cash=abs(out_cash_remained), title=title)
+        elif cash_remained == 0:
+            return self.STAY_STRONG
+        return self.YOU_HAVE_MONEY.format(
+            cash=out_cash_remained, title=title)
 
 
 if __name__ == '__main__':
     cash_calc = CashCalculator(5000)
     cash_calc.add_record(Record(3300, 'Поход в кабак', '12.05.2021'))
     cash_calc.add_record(Record(5100, 'Такси', '07.05.2021'))
-    cash_calc.add_record(Record(1000, 'Мойка'))
+    cash_calc.add_record(Record(1432, 'Мойка'))
     cash_calc.add_record(Record(500, 'Заправка', '06.05.2021'))
     calor_calc = CaloriesCalculator(1500)
     calor_calc.add_record(Record(300, 'Омлет', '09.05.2021'))
@@ -97,5 +95,5 @@ if __name__ == '__main__':
     calor_calc.add_record(Record(450, 'Пельмешки', '05.05.2021'))
     print(cash_calc.get_today_stats())
     print(cash_calc.get_week_stats())
-    print(cash_calc.get_today_cash_remained('rub'))
+    print(cash_calc.get_today_cash_remained('eur'))
     print(calor_calc.get_calories_remained())
